1a3947f6ccf7c7e689644544e82ac95c40cb5ac0
aleixvilaseca <aleixvilaseca@gmail>
2022-12-20 17:27:11 +0100

    extract all projects and iterate for severity

--- main.py
+++ main.py
@@ -1,46 +1,61 @@
 import requests
-from os.path import exists
 import json
 import sys
-from openpyxl.workbook import workbook
+import constants
 import openpyxl
 import os
 import urllib3
 import pyslurpers
+from sonarqube import SonarQubeClient
+from openpyxl.workbook import workbook
+from requests.auth import HTTPBasicAuth
+from os.path import exists
+from django.http import HttpResponse
 
-# Get the data from SonarQube
-"""username = 'admin'
-password = 'CroLeveStM'
 
-api_url_blocker = "https://codi.qualitat.solucions.gencat.cat/api/issues/search?componentKeys=0434-gsitgf&types=VULNERABILITY&severities=BLOCKER"
+def get_severities(request, key_list, sonar): 
 
-data_file = open("reports/blockers.json", "w") # write the security data to a file so we can use it later
-data_file.write(json.dumps(api_url_blocker))
-data_file.close()
+    print('Connection to Sonarqube')
+    url = "https://codi.qualitat.solucions.gencat.cat/"
+    username = constants.SONAR_USER
+    password = constants.SONAR_PASSWORD
+    sonar = SonarQubeClient(url, username, password)
 
-api_url_critical = "https://codi.qualitat.solucions.gencat.cat/api/issues/search?componentKeys=0434-gsitgf&types=VULNERABILITY&severities=CRITICAL"
+    print('Getting all projects')
+    sonar = list(sonar.projects.search_projects()) # search all sonarqube projects
+        
+    key_list = []
+    for key in sonar:
+        key_list.append(key['key']) # append the project key to a list
 
-data_file = open("reports/criticals.json", "w") # write the security data to a file so we can use it later
-data_file.write(json.dumps(api_url_critical))
-data_file.close()"""
+    metric_file = open("reports/project_list.json", "w") # write the security data to a file so we can use it later
+    metric_file.write(json.dumps(key_list))
+    metric_file.close()
 
-def sonarRest(url,method):
-    username = 'admin'
-    password = 'CroLeveStM'
+    key_list = str(key_list).replace('[', '').replace(']', '').replace("'", "").replace(" ", "") # remove the brackets from the list (not needed for the API
+    key_list = key_list.split(',') # split the list into a list of strings
 
-    #api_url_blocker = "https://codi.qualitat.solucions.gencat.cat/api/issues/search?componentKeys=0434-gsitgf&types=VULNERABILITY&severities=BLOCKER"
-    #api_url_critical = "https://codi.qualitat.solucions.gencat.cat/api/issues/search?componentKeys=0434-gsitgf&types=VULNERABILITY&severities=CRITICAL"
+    try:
+        for key in key_list:
+            if key is not None:
+                api_url_sev = requests.get("https://codi.qualitat.solucions.gencat.cat/api/issues/search?componentKeys=%s&types=VULNERABILITY&severities=BLOCKER,CRITICAL" %(key), auth=HTTPBasicAuth(username, password))
+                api_url_sev.json()
 
-    api_url_blocker = sonarRest('https://codi.qualitat.solucions.gencat.cat/api/issues/search?types=VULNERABILITY&severities=BLOCKER', 'GET')
-    total = (api_url_blocker.total.toFloat()/100).round()
+            """data_file = open("reports/severities1.json", "w") # write the security data to a file so we can use it later
+            data_file.write(json.dumps(api_url_sev.json()))
+            data_file.close()"""
 
-    counter = 1
+    except Exception as error:
+        print(error)
 
-    while(counter <= total):
-        api_url_blocker = sonarRest("https://codi.qualitat.solucions.gencat.cat/api/issues/search?types=VULNERABILITY&severities=BLOCKER", 'GET')
-        print(api_url_blocker)
-        counter+1
+    """api_url_sev = list(api_url_sev)
+    total = []
+    for total in api_url_sev:
+        total.append(total['total']), ",".join(total, key)"""
 
-        data_file = open("reports/criticals.json", "w") # write the security data to a file so we can use it later
-        data_file.write(json.dumps(api_url_blocker))
-        data_file.close()
+    
+
+
+if __name__ == "__main__":
+
+    get_severities(request=None, key_list=['key'], sonar=None)
