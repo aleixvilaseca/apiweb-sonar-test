5029f3733ed50cc5fcf5f9a830ddc7b1b4b10a16
aleixvilaseca <aleixvilaseca@gmail>
2022-12-27 08:16:12 +0100

    update more request from api sonar

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from os.path import exists
 from django.http import HttpResponse
 
 
-def get_severities(request, key_list, sonar, total): 
+def get_severities(request, key_list, sonar, total, measure): 
     
     """Get the number of vulnerabilities per project and write them to a file"""
     print('Connection to Sonarqube')
@@ -31,9 +31,50 @@ def get_severities(request, key_list, sonar, total):
 
     key_list = str(key_list).replace('[', '').replace(']', '').replace("'", "").replace(" ", "").split(',') # remove the brackets from the list (not needed for the API
 
-    print('Getting the number of vulnerabilities per project')
-
     try:
+
+        for key in key_list:
+            if key is not None:
+                
+                api_url_sev = requests.get("https://codi.qualitat.solucions.gencat.cat/api/measures/component?component=%s&metricKeys=security_rating&ps=100" %(key), auth=HTTPBasicAuth(username, password))
+
+                if api_url_sev.status_code == 200:
+                    data = api_url_sev.json()
+                    measure = data['component']['measures'] # get the value of the metric
+                    for value in measure:
+                        measure = value['value'].replace('1.0', 'A').replace('2.0', 'B').replace('3.0', 'C').replace('4.0', 'D').replace('5.0', 'E') # replace the letter with a number
+
+                        if measure == 'A':
+                           measure = "100"
+                        elif measure == 'B':
+                            measure = "70"
+                        elif measure == 'C':
+                            measure = "40"
+                        elif measure == 'D':
+                            measure = "20"
+                        elif measure == 'E':
+                            measure = "0"
+
+                    with open('reports/security-rating.txt', 'a') as f:
+                        f.write(' %s: %s \r      ' %(key, measure))    
+                else:
+                    print('Error: %s' %(api_url_sev.status_code))  
+
+
+        for key in key_list:
+            if key is not None:
+                
+                api_url_sev = requests.get("https://codi.qualitat.solucions.gencat.cat/api/issues/search?componentKeys=%s&types=VULNERABILITY&ps=100" %(key), auth=HTTPBasicAuth(username, password))
+
+                if api_url_sev.status_code == 200:
+                    data = api_url_sev.json()
+                    total = data['total']
+                    with open('reports/vulnerabilities.txt', 'a') as f:
+                        f.write(' %s: %s \r      ' %(key, total))    
+                else:
+                    print('Error: %s' %(api_url_sev.status_code))  
+
+
         for key in key_list:
             if key is not None:
                 
@@ -58,12 +99,38 @@ def get_severities(request, key_list, sonar, total):
                     with open('reports/vulnerabilities-critical.txt', 'a') as f:
                         f.write(' %s: %s \r      ' %(key, total))    
                 else:
-                    print('Error: %s' %(api_url_sev.status_code))               
+                    print('Error: %s' %(api_url_sev.status_code))       
+
+        for key in key_list:
+            if key is not None:
                 
+                api_url_sev = requests.get("https://codi.qualitat.solucions.gencat.cat/api/issues/search?componentKeys=%s&types=CODE_SMELL,BUG,VULNERABILITY&severities=BLOCKER&ps=100" %(key), auth=HTTPBasicAuth(username, password))
+
+                if api_url_sev.status_code == 200:
+                    data = api_url_sev.json()
+                    total = data['total']
+                    with open('reports/total-severities-blocker.txt', 'a') as f:
+                        f.write(' %s: %s \r      ' %(key, total))    
+                else:
+                    print('Error: %s' %(api_url_sev.status_code))     
+
+        for key in key_list:
+            if key is not None:
+                
+                api_url_sev = requests.get("https://codi.qualitat.solucions.gencat.cat/api/issues/search?componentKeys=%s&types=CODE_SMELL,BUG,VULNERABILITY&severities=CRITICAL&ps=100" %(key), auth=HTTPBasicAuth(username, password))
+
+                if api_url_sev.status_code == 200:
+                    data = api_url_sev.json()
+                    total = data['total']
+                    with open('reports/total-severities-critical.txt', 'a') as f:
+                        f.write(' %s: %s \r      ' %(key, total))    
+                else:
+                    print('Error: %s' %(api_url_sev.status_code)) 
+            
                         
     except Exception as error:
         print(error)
     
 if __name__ == "__main__":
 
-    get_severities(request=None, key_list=['key'], sonar=None, total=['total'])
+    get_severities(request=None, key_list=['key'], sonar=None, total=['total'], measure=['measure'])
